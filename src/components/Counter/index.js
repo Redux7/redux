3076216e62a192c1ef6bbe338c00854e088d5595
americanman <snowboardracer7@gmail.com>
2018-11-22 19:16:25 -0500

    Add Counter component

---
+++ src/components/Counter/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <div>
+        <p>Yooooooo</p>
+      </div>
+    )
+  }
+}
+
+export default Counter;
